414b7ecd6608cdc5412acf75d7407ee5e70a9464
Wolfgang Mueller-Friedt <wolfmuel@de.ibm.com>
2022-03-21 17:07:57 +0100

    adding lowHCPI

--- hhs/views.py
+++ hhs/views.py
@@ -37,7 +37,7 @@ def index(request):
 
 	context = {
 		'latest_list': latest_list,
-		'HCPI': HHSEntry.get_curhcpi(request.user),
+		'HCPI': HHSEntry.get_hcpi(request.user),
 		'best': HHSEntry.get_best(request.user),
 	}
 	return HttpResponse(template.render(context, request))
