2ab0837aadec3ab6b85996a9f31344cb7ec88f51
Wolfgang Mueller-Friedt <wolfmuel@de.ibm.com>
2022-05-03 10:18:40 +0200

    adapting callall script

--- calcallhcpi.py
+++ calcallhcpi.py
@@ -1,6 +1,10 @@
 from hhs.models import HHSEntry
 
-ds = HHSEntry.objects.filter(player='jessica')
-for i in ds:
-	print(i)
+ds = HHSEntry.objects.filter(player='wolfmuel').order_by('-date')
+l = len(ds)
+r = l-20
+while r > 0:
+	print(ds[r:r+20])
+	r = r -20
+
 
