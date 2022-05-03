2e4a20a4f95d97bf8a0ed3d94060f03b5a17aa68
Wolfgang Mueller-Friedt <wolfmuel@de.ibm.com>
2022-05-03 10:38:04 +0200

    adapting callall script

--- calcallhcpi.py
+++ calcallhcpi.py
@@ -4,7 +4,7 @@ ds = HHSEntry.objects.filter(player='wolfmuel').order_by('-date')
 l = len(ds)
 r = 0
 for i in range(0, l-20):
-	li = ds[i:20]
+	li = ds[i:i+20]
 	st = "chunk: "+ str(i)
 	for s in li:
 		st += " " + str(s.score)
