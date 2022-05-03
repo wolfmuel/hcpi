1580170ae63c1393fd1a17e67b72b096de9444b7
Wolfgang Mueller-Friedt <wolfmuel@de.ibm.com>
2022-05-03 10:40:10 +0200

    adapting callall script

--- calcallhcpi.py
+++ calcallhcpi.py
@@ -3,7 +3,7 @@ from hhs.models import HHSEntry
 ds = HHSEntry.objects.filter(player='wolfmuel').order_by('-date')
 l = len(ds)
 r = 0
-for i in range(0, l-21):
+for i in range(0, l-19):
 	li = ds[i:i+20]
 	st = "chunk: "+ str(i)
 	for s in li:
