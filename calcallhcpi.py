3bc8f2ae59027018151e117a583df0fa459ad940
Wolfgang Mueller-Friedt <wolfmuel@de.ibm.com>
2022-05-03 10:45:16 +0200

    adapting callall script

--- calcallhcpi.py
+++ calcallhcpi.py
@@ -1,13 +1,33 @@
+import math
 from hhs.models import HHSEntry
+
+
+def round_half_up(n, decimals=0):
+	multiplier = 10 ** decimals
+	return math.floor(float(n)*multiplier + 0.5) / multiplier
+
+def round_up(n):
+	a = round_half_up(n, 2)
+	return round_half_up(a, 1)
+
+def get_hcpi(li):
+	l = []
+	h = 0
+	for i in li:
+		l.append(i.sd)
+		l.sort()
+	if len(l) >= 8:
+		for i in l[:8]:
+			h = h + i
+		return round_up(h / 8)
+	return 0
 
 ds = HHSEntry.objects.filter(player='wolfmuel').order_by('-date')
 l = len(ds)
 r = 0
 for i in range(0, l-19):
 	li = ds[i:i+20]
-	st = "chunk: "+ str(i)
-	for s in li:
-		st += " " + str(s.score)
-	print(st)
+	hcpi = get_hcpi(li)
+	print(hcpi)
 		
 
