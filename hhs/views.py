c07fa1bb9a495f0e6f144fde87b91772d000b1af
Wolfgang Mueller-Friedt <wolfmuel@de.ibm.com>
2022-09-18 14:13:17 +0200

    added download

--- hhs/views.py
+++ hhs/views.py
@@ -157,7 +157,7 @@ def graph(request):
 
 
 def download(request):
-	os.makedirs('/download/'+request.user, exist_ok=True)
+	os.makedirs('/download/'+str(request.user), exist_ok=True)
 	return HttpResponseRedirect('/hhs')
 
 		
